6e028f0f67d3047a4f645b2e5debed49b4fbee6c
MegiiL <127933206+MegiiL@users.noreply.github.com>
2024-12-06 12:58:25 +0100

    Update ghost.js

--- ghost.js
+++ ghost.js
@@ -1,6 +1,6 @@
 class Ghost{
     constructor(x, y, width, height, speed, imageX, imageY, imageWidth, imageHeight, range){
-        this.x = x; // x positio
+        this.x = x; // x position
         this.y = y; // y position middle of map
         this.width = width; //ghost width in canvas
         this.height = height; //ghost height in canvas
